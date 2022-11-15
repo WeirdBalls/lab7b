efdb0f2693a02a92a1c886d519afadaa79b8f81f
WeirdBalls <22877622+WeirdBalls@users.noreply.github.com>
2022-11-15 22:38:02 +0800

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env sh
 
 set -x
-docker run -d -p 80:80 --name my-apache-php-app -v /home/fee/SSD/Jenkins-Test//src:/var/www/html php:7.2-apache
+docker run -d -p 80:80 --name my-apache-php-app -v /home/lab7b/src:/var/www/html php:7.2-apache
 sleep 1
 set +x
 
 echo 'Now...'
-echo 'Visit http://localhost to see your PHP application in action.'
+echo 'Visit http://localhost to see your PHP application in action.'s
